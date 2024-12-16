4aa066ef0036c0eea3689886ac3a18f097b66c7e
Cavidan03 <aa4060918@gmail.com>
2024-12-16 13:03:03 +0400

    Changes the port

--- portfolio/connect.php
+++ portfolio/connect.php
@@ -1,7 +1,7 @@
 <?php
 
 try{
-  $db=new PDO("mysql:host=localhost;dbname=myportfolio;charset-utf8","root","");
+  $db=new PDO("mysql:host=localhost;port=4306;dbname=myportfolio;charset-utf8","root","");
 
 } catch(PDOException $e){
   echo $e->getMessage();  
